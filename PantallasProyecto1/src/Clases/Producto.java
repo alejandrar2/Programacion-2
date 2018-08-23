5c5aa2fd169e4e3d24f7f6fb83929c666baf2cae
alejandrar2 <40775960+alejandrar2@users.noreply.github.com>
2018-08-22 20:28:09 -0600

    Clases
    Clases que se usaran en el proyecto y cambio en la pantalla

---
+++ PantallasProyecto1/src/Clases/Producto.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author DELL
+ */
+public class Producto {
+    private int id;
+    private int precio;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(int id, int precio, int cantidad) {
+        this.id = id;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", precio=" + precio + ", cantidad=" + cantidad + '}';
+    }
+    
+    
+
+    
+}
